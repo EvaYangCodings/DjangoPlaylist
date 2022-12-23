8ad29204761d1de9755f5f13559dc470d3316af2
Eva Yang <jiehu@Jies-MacBook-Pro.local>
2022-12-23 09:42:30 -0500

    create the home and songs templates

---
+++ first_project/zing_it/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from zing_it import views
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('playlist/<int:id>', views.playlist, name="playlist"),
+]
