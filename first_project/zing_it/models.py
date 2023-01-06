ed63a1d1df243c5894524cfbf5db39e05a28ee26
Eva Yang <jiehu@Jies-MacBook-Pro.local>
2023-01-06 15:46:22 -0500

    build the many_to_many relationship between the two models

--- first_project/zing_it/models.py
+++ first_project/zing_it/models.py
@@ -6,6 +6,7 @@ class Song(models.Model):
     artist = models.CharField(max_length=50, unique= True)
     album = models.CharField(max_length=70)
     length = models.TimeField()
+    playlist_name = models.ManyToManyField('Playlist')
 
 class Playlist(models.Model):
     name = models.CharField(max_length=70, unique= True)
