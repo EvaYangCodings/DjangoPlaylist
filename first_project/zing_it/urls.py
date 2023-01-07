0e9d993f45e37e962739a449967cd829c3bcdcc5
Eva Yang <jiehu@Jies-MacBook-Pro.local>
2023-01-07 18:49:57 -0500

    add a login path

--- first_project/zing_it/urls.py
+++ first_project/zing_it/urls.py
@@ -6,5 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.home, name="home"),
     path('signup/', views.signup, name="signup"),
+    path('login/', views.login, name="login"),
     path('playlist/<int:id>', views.playlist, name="playlist"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
